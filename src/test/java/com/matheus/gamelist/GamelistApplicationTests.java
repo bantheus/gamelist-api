c9e2f461e213962b6b24ab34833af25b1f32be56
Matheus <matheuschmidt.b@gmail.com>
2025-05-20 11:09:28 -0300

    feat: add Spring Boot project with Maven

---
+++ src/test/java/com/matheus/gamelist/GamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
