95b034663395fee6f147dfd8f0c27e69b617b713
Matheus <matheuschmidt.b@gmail.com>
2025-05-20 16:24:56 -0300

    feat: add GameService with a method to fetch all games

---
+++ src/main/java/com/matheus/gamelist/services/GameService.java
@@ -0,0 +1,21 @@
+package com.matheus.gamelist.services;
+
+import com.matheus.gamelist.dto.GameMinDTO;
+import com.matheus.gamelist.entities.Game;
+import com.matheus.gamelist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(GameMinDTO::new).toList();
+    }
+}
