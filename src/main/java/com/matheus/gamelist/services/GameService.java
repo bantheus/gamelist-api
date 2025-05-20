75d4507daf7f8de49add5c15446eb7031fd09523
Matheus <matheuschmidt.b@gmail.com>
2025-05-20 17:48:34 -0300

    feat: add findById method to GameService

--- src/main/java/com/matheus/gamelist/services/GameService.java
+++ src/main/java/com/matheus/gamelist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.matheus.gamelist.services;
 
+import com.matheus.gamelist.dto.GameDTO;
 import com.matheus.gamelist.dto.GameMinDTO;
 import com.matheus.gamelist.entities.Game;
 import com.matheus.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,8 +16,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long gameId) {
+        Game result = gameRepository.findById(gameId).get();
+        return new GameDTO(result);
     }
 }
