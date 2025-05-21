d3d9f9dbdaa1285e9b8b58fe26361e015188b536
Matheus <matheuschmidt.b@gmail.com>
2025-05-21 09:42:09 -0300

    feat: add GameListDTO

---
+++ src/main/java/com/matheus/gamelist/dto/GameListDTO.java
@@ -0,0 +1,25 @@
+package com.matheus.gamelist.dto;
+
+import com.matheus.gamelist.entities.GameList;
+
+public class GameListDTO {
+
+    private Long id;
+    private String name;
+
+    public GameListDTO() {
+    }
+
+    public GameListDTO(GameList entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
