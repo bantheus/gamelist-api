62e156dffcaa9e426ee2ed7221b82f24da14fda7
Matheus <matheuschmidt.b@gmail.com>
2025-05-21 10:33:34 -0300

    feat: add endpoint to fetch games by list ID

--- src/main/java/com/matheus/gamelist/controllers/GameListController.java
+++ src/main/java/com/matheus/gamelist/controllers/GameListController.java
@@ -1,9 +1,12 @@
 package com.matheus.gamelist.controllers;
 
 import com.matheus.gamelist.dto.GameListDTO;
+import com.matheus.gamelist.dto.GameMinDTO;
 import com.matheus.gamelist.services.GameListService;
+import com.matheus.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +19,16 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll() {
         return gameListService.findAll();
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable("listId") Long listId) {
+        return gameService.findByList(listId);
     }
 }
