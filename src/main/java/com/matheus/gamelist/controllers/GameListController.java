9d021234b4ef74d594d96599adfe66e3ea0f17cb
Matheus <matheuschmidt.b@gmail.com>
2025-05-21 09:51:27 -0300

    feat: add GameListController with an endpoint to fetch all game lists

---
+++ src/main/java/com/matheus/gamelist/controllers/GameListController.java
@@ -0,0 +1,23 @@
+package com.matheus.gamelist.controllers;
+
+import com.matheus.gamelist.dto.GameListDTO;
+import com.matheus.gamelist.services.GameListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/lists")
+public class GameListController {
+
+    @Autowired
+    private GameListService gameListService;
+
+    @GetMapping
+    public List<GameListDTO> findAll() {
+        return gameListService.findAll();
+    }
+}
