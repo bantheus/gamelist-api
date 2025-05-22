c79c6d100e5e011cae2118deb20b1db66e8c58c7
Matheus <matheuschmidt.b@gmail.com>
2025-05-22 17:00:27 -0300

    feat: add endpoint to handle game list item replacement

--- src/main/java/com/matheus/gamelist/controllers/GameListController.java
+++ src/main/java/com/matheus/gamelist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package com.matheus.gamelist.controllers;
 
 import com.matheus.gamelist.dto.GameListDTO;
 import com.matheus.gamelist.dto.GameMinDTO;
+import com.matheus.gamelist.dto.ReplacementDTO;
 import com.matheus.gamelist.services.GameListService;
 import com.matheus.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +28,10 @@ public class GameListController {
     @GetMapping(value = "/{listId}/games")
     public List<GameMinDTO> findByList(@PathVariable("listId") Long listId) {
         return gameService.findByList(listId);
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable("listId") Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
