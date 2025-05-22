4fd0f9b8ecd1a7e0ab25fc543186041038ec653d
Matheus <matheuschmidt.b@gmail.com>
2025-05-22 15:53:39 -0300

    feat: add method to reorder games within a list

--- src/main/java/com/matheus/gamelist/services/GameListService.java
+++ src/main/java/com/matheus/gamelist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.matheus.gamelist.services;
 
 import com.matheus.gamelist.dto.GameListDTO;
 import com.matheus.gamelist.entities.GameList;
+import com.matheus.gamelist.projections.GameMinProjection;
 import com.matheus.gamelist.repositories.GameListRepository;
+import com.matheus.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +17,27 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(GameListDTO::new).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection game = list.remove(sourceIndex);
+        list.add(destinationIndex, game);
+
+        int min = Math.min(sourceIndex, destinationIndex);
+        int max = Math.max(sourceIndex, destinationIndex);
+
+        for (int position = min; position <= max; position++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(position).getId(), null);
+        }
     }
 }
